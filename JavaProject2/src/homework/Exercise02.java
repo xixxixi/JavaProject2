43fed318e241995f1dd9cc3f23ef2f8acc409af1
admin <admin@YD01-14>
2021-02-22 14:01:35 +0900

    git

---
+++ JavaProject2/src/homework/Exercise02.java
@@ -0,0 +1,13 @@
+package homework;
+
+public class Exercise02 {
+
+	public static void main(String[] args) {
+		int x = 10;
+		int y = 20;
+		int z = (++x) + (y--);
+		System.out.println(z);
+
+	}
+
+}
---
+++ JavaProject2/src/homework/Exercise03.java
@@ -0,0 +1,12 @@
+package homework;
+
+public class Exercise03 {
+
+	public static void main(String[] args) {
+		int score = 85;
+		String result = (!(score>90))? "°¡":"³ª";
+		System.out.println(result);
+
+	}
+
+}
