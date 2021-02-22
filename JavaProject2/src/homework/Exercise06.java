0ef18decb4d543e519f06e94fca51bec1ad7f90b
XXX <XXX@DESKTOP-SVVQM6V>
2021-02-22 19:39:55 +0900

    2/22 19:40

---
+++ JavaProject2/src/homework/Exercise06.java
@@ -0,0 +1,14 @@
+package homework;
+
+public class Exercise06 {
+
+	public static void main(String[] args) {
+		int lengthTop = 5;
+		int lengthBottom = 10;
+		int height = 7;
+		double area = (0.5 * (lengthTop + lengthBottom) * height);
+		System.out.println(area);
+
+	}
+
+}
